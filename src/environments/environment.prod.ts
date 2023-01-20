edc2fb4c40e4c4938c062eeb357165d582aedd63
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2023-01-20 20:00:29 +0100

    fix(): env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,24 @@
 const baseUrlApp: string = '../../';
+const host: string = 'http://synologynas.ddns.net:8000';
 
 export const environment = {
   production: true,
   baseUrl: '../../',
+  version: '1.0.0',
 
+  // Mock Data
   getUserUrl: baseUrlApp + 'assets/core/mock/user.mock.json',
-  getDashboardDataUrl: baseUrlApp + 'assets/core/mock/dashboard.mock.json',
+  getDashboardDataUrlMock: baseUrlApp + 'assets/core/mock/dashboard.mock.json',
+  getWalletDataUrl: baseUrlApp + 'assets/core/mock/wallets.mock.json',
+  getResumeDataUrlMock: baseUrlApp + 'assets/core/mock/stats.mock.json',
+
+  // Datas
+  registerDataUrl: host + '/v1/auth/sign-up',
+  loginDataUrl: host + '/v1/auth/login',
+  addUpdateWalletDataUrl: host + '/v1/wallet/insert-update',
+  listWalletDataurl: host + '/v1/wallet/list',
+  getDashboardDataUrl: host + '/v1/app/dashboard',
+  getResumeDataUrl: host + '/v1/app/resume',
+  addStatsDataUrl: host + '/v1/app/add/stats',
+  forgotPassword: false,
 };
