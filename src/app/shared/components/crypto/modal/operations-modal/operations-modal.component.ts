e77aead101290ac0dc83928615bb7a77eefc10e1
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2023-05-25 23:45:01 +0200

    fix(): Operation add stats implementation

--- src/app/shared/components/crypto/modal/operations-modal/operations-modal.component.ts
+++ src/app/shared/components/crypto/modal/operations-modal/operations-modal.component.ts
@@ -1,16 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Asset } from 'src/assets/core/data/class/crypto.class';
+import { Wallet } from 'src/assets/core/data/class/dashboard.class';
 
 @Component({
   selector: 'app-operations-modal',
   templateUrl: './operations-modal.component.html',
-  styleUrls: ['./operations-modal.component.scss']
+  styleUrls: ['./operations-modal.component.scss'],
 })
 export class OperationsModalComponent implements OnInit {
   @Input('modalId') modalId: string = '';
+  @Input('wallets') wallets: Wallet[] = [];
+  @Input('assets') assets: Asset[] = [];
+  @Input('currency') currency: string = '';
 
-  constructor() { }
+  isAddStatsSelected: boolean = false;
 
-  ngOnInit(): void {
+  currentIndex: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  changeAsset() {
+    this.currentIndex += 1;
   }
 
+  filterWallets(wallets: Wallet[], assetName: string): Wallet[] {
+    let wallAsset = wallets.filter((w) =>
+      w.assets.find((a) => a.name == assetName)
+    );
+    /*wallAsset.forEach((w) => {
+      w.assets = w.assets.filter((a) => a.name == assetName);
+    });*/
+    return wallAsset;
+  }
 }
