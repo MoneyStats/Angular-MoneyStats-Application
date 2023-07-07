4f86bb8733bf29e5785c4ea3eb2df86629f02093
Giovanni <giovannilamarmora.working@gmail.com>
2023-07-07 17:59:12 +0200

    feat(): added operation and bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   baseUrlSettings: '../../../../../' + subDomain,
   baseUrlDashboard: '../../../' + subDomain,
   baseUrlVersion: '../../../../../../' + subDomain,
-  version: '1.7.0 (Beta)',
+  version: '1.7.1 (Beta)',
 
   // Mock Data
   getUserUrl: baseUrlApp + 'assets/core/mock/user.mock.json',
