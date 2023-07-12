f44b387a44148c6a32657e5fed765c916a26223d
Giovanni <giovannilamarmora.working@gmail.com>
2023-07-12 22:07:31 +0200

    feat(): Crypto select

---
+++ src/app/shared/components/crypto/asset-select/asset-select.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AssetSelectComponent } from './asset-select.component';
+
+describe('AssetSelectComponent', () => {
+  let component: AssetSelectComponent;
+  let fixture: ComponentFixture<AssetSelectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AssetSelectComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AssetSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
