c63024a3b18eb69bd4f0af868a19666e86d02872
Giovanni <giovannilamarmora.working@gmail.com>
2023-07-20 17:41:35 +0200

    feat() Added Opertion in resume

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   baseUrlSettings: '../../../../../' + subDomain,
   baseUrlDashboard: '../../../' + subDomain,
   baseUrlVersion: '../../../../../../' + subDomain,
-  version: '1.8.5 (Beta)',
+  version: '1.8.6 (Beta)',
 
   // Mock Data
   getUserUrl: baseUrlApp + 'assets/core/mock/user.mock.json',
