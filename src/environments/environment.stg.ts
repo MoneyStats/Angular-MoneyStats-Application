176e5c3179eb6451056be21030dffb811ec85445
Giovanni <giovannilamarmora.working@gmail.com>
2023-11-03 19:10:04 +0100

    feat(): Change Host, bump 1.11.3

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,5 +1,5 @@
 const baseUrlApp: string = '../../';
-const host: string = 'http://stg.moneystats.service.hostwebserver.site';
+const host: string = 'https://stg.moneystats.service.hostwebserver.site';
 //const host: string = 'https://oraclewebserver.ddns.net/stg-moneystats-service';
 //const subDomain = 'stg-moneystats/';
 const subDomain = '';
