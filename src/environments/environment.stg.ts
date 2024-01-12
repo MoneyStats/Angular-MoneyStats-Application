b5fd210b2dbae8845baf10834d87daaf5f20ab46
Giovanni <giovannilamarmora.working@gmail.com>
2024-01-12 18:02:11 +0100

    feat(): Fixed Scroll, bump 1.16.22

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -13,7 +13,7 @@ export const environment = {
   baseUrlSettings: '../../../../../' + subDomain,
   baseUrlDashboard: '../../../' + subDomain,
   baseUrlVersion: '../../../../../../' + subDomain,
-  version: '1.16.21 (Beta)',
+  version: '1.16.22 (Beta)',
   hostService: host + subDomain,
 
   // Mock Data
